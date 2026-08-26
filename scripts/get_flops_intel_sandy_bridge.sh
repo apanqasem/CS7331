43eeca8842a2a2937ba1272c9e48c20d54fb1ce1
Apan Qasem <aq10@txstate.edu>
2026-08-26 18:50:47 +0000

    updated script GFLOP-SandyBridge

--- scripts/get_flops_intel_sandy_bridge.sh
+++ scripts/get_flops_intel_sandy_bridge.sh
@@ -2,21 +2,11 @@
 
 prog_with_args=$1
 
-
-
 # dump perf out to tmp file 
-perf stat -e r538010 ${prog_with_args} 2> tmp.prof
+perf stat -M GFLOPS ${prog_with_args} 2> tmp.prof
 
-# extract flop count
-flops=`cat tmp.prof | grep r538010  | awk '{printf $1}' | sed 's/,//g'`
-flops=`echo $flops | awk '{print $1 / 2.5}'`
-
-
-# extract number of seconds 
-secs=`cat tmp.prof | grep "elapsed"  | awk '{print $1}'`
-
-# calculate FLOPS
-FLOPS=`echo $flops $secs | awk '{printf "%3.2f", ($1/1e+09)/$2}'`
+gflops=`cat tmp.prof | grep "scalar_single" | awk '{print $4}'`
 
 # print the result
-echo "Performance = $FLOPS GFLOPS/s"
+echo "Performance = $gflops GFLOPS/s"
+
